2eebb1b00858ca867b57d4dbbd54583b8b7156f9
Matthew-B549 <matei.bennett@gmail.com>
2026-05-15 15:53:59 -0400

    Worked on the initial login system. [UNFINISHED]
    Comments show my thought process

---
+++ trialLoginSystem.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       //First you have to login. Once you login, it should recognize your name (some accounts will be default included) and welcome you
+       //Next it should also recognize your access level -> do you have access to change and edit everything or is it only to see your credentials
+       //Schedule vacations, enter hours worked, see next payday (it would be cool if it could automatically get the date of today)
+       //Emphasize choice -> the prior system I created led you through different things -> you couldn't choose if you wanted to do this or that
+    login()
+
+
+
+    }
+    
+    public static char menu(){
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
+class loginInformation{
+    //What do I actually want to return with this? I am using an object to be able to return and access multiple values
+    //1. A boolean value that says if login has actually been successful (that there is a password match). 2. The ID for the person logging in. 3. The access level of the person logging in (administrator, hiring manager, employee, etc.)
+    loginInformation(){
+        this.//UNFINISHED
+    }
+    
+    
+}
